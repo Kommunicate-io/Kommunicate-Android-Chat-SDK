603280b59a6d4ef82824dfc989e8fb3a3da90bdc
Cati Rísquez <kathiiii83@gmail.com>
2018-07-02 17:06:01 +0200

    Refactor KommunicateApplication class

--- app/src/main/java/kommunicate/io/sample/KommunicateApplication.java
+++ app/src/main/java/kommunicate/io/sample/KommunicateApplication.java
@@ -14,15 +14,13 @@ import com.applozic.mobicomkit.uiwidgets.uilistener.KmActionCallback;
 import java.util.ArrayList;
 import java.util.List;
 
-import io.kommunicate.KmException;
 import io.kommunicate.KmHelper;
 import io.kommunicate.Kommunicate;
-
+import io.kommunicate.app.BuildConfig;
 
 /**
  * Created by ashish on 23/01/18.
  */
-
 public class KommunicateApplication extends MultiDexApplication implements KmActionCallback, ALRichMessageListener {
 
     @Override
@@ -41,23 +39,18 @@ public class KommunicateApplication extends MultiDexApplication implements KmAct
 
         switch (action) {
             case Kommunicate.START_NEW_CHAT:
-                //Kommunicate.startOrGetConversation(context, "testClientGroupId", "reytum@live.com", null, "My Group");
-                //Kommunicate.setStartNewChat(context, "vipin+testkm01012018@applozic.com", "Hotel-Booking-Assistant"); //pass null if you want to use default bot
                 List<String> agents = new ArrayList<>();
-                agents.add("reytum@live.com");
+                agents.add(BuildConfig.KOMMUNICATE_AGENT);
                 List<String> bots = new ArrayList<>();
-                //Kommunicate.setStartNewChat(context, "reytum@live.com", "Hotel-Booking-Assistant");
                 try {
                     KmHelper.setStartNewChat(context, agents, bots);
-                    //Kommunicate.startNewConversation(context, null, agents, bots,false, null);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-                //KmHelper.setStartNewUniqueChat(context, agents, bots);
                 break;
 
             case Kommunicate.LOGOUT_CALL:
-                KmHelper.performLogout(context, object); //object will receive the exit Activity, the one that will be launched when logout is successfull
+                KmHelper.performLogout(context, object);
                 break;
         }
     }

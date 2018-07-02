c0140d63646f38e85e795d31d0d4efd67810920c
Cati Rísquez <kathiiii83@gmail.com>
2018-07-02 17:08:39 +0200

    Refactor code in app module

--- app/src/main/java/kommunicate/io/sample/SplashScreenActivity.java
+++ app/src/main/java/kommunicate/io/sample/SplashScreenActivity.java
@@ -17,8 +17,6 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-
-
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {

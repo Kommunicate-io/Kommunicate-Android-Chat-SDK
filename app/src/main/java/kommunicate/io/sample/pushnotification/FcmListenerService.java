2b612d43297b804e86f51f037e2d70f4f060510d
Cati Rísquez <kathiiii83@gmail.com>
2018-07-25 16:09:46 +0200

    Remove deprecated FirebaseInstanceIdService, update and refactor code

--- app/src/main/java/kommunicate/io/sample/pushnotification/FcmListenerService.java
+++ app/src/main/java/kommunicate/io/sample/pushnotification/FcmListenerService.java
@@ -1,22 +1,35 @@
 package kommunicate.io.sample.pushnotification;
 
-
 import android.util.Log;
 
+import com.applozic.mobicomkit.Applozic;
+import com.applozic.mobicomkit.api.account.register.RegisterUserClientService;
+import com.applozic.mobicomkit.api.account.user.MobiComUserPreference;
 import com.applozic.mobicomkit.api.notification.MobiComPushReceiver;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
-
 
 public class FcmListenerService extends FirebaseMessagingService {
 
     private static final String TAG = "KommunicatePushReceiver";
 
     @Override
+    public void onNewToken(String newToken) {
+        super.onNewToken(newToken);
+        Log.i(TAG, "Found Registration Id:" + newToken);
+        Applozic.getInstance(this).setDeviceRegistrationId(newToken);
+        if (MobiComUserPreference.getInstance(this).isRegistered()) {
+            try {
+                new RegisterUserClientService(this).updatePushNotificationId(newToken);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-
         Log.i(TAG, "Message data:" + remoteMessage.getData());
-
         if (remoteMessage.getData().size() > 0) {
             if (MobiComPushReceiver.isMobiComPushNotification(remoteMessage.getData())) {
                 Log.i(TAG, "Kommunicate notification processing...");
@@ -24,7 +37,5 @@ public class FcmListenerService extends FirebaseMessagingService {
                 return;
             }
         }
-
     }
-
 }

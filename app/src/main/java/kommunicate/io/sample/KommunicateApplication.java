d75a0213d97ac657c5e172d78fbf40de571300c7
Ashish <reytum@live.com>
2018-10-11 21:30:57 +0530

    update version to 1.6.1

--- app/src/main/java/kommunicate/io/sample/KommunicateApplication.java
+++ app/src/main/java/kommunicate/io/sample/KommunicateApplication.java
@@ -1,21 +1,15 @@
 package kommunicate.io.sample;
 
 import android.content.Context;
-import android.content.Intent;
 import android.support.multidex.MultiDex;
 import android.support.multidex.MultiDexApplication;
 
 import com.applozic.mobicomkit.api.account.user.User;
-import com.applozic.mobicomkit.api.conversation.Message;
-import com.applozic.mobicomkit.api.conversation.MessageBuilder;
-import com.applozic.mobicomkit.uiwidgets.conversation.richmessaging.ALRichMessageListener;
-import com.applozic.mobicomkit.uiwidgets.conversation.richmessaging.payment.PaymentActivity;
 import com.applozic.mobicomkit.uiwidgets.uilistener.KmActionCallback;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import io.kommunicate.KmException;
 import io.kommunicate.KmHelper;
 import io.kommunicate.Kommunicate;
 
@@ -24,7 +18,7 @@ import io.kommunicate.Kommunicate;
  * Created by ashish on 23/01/18.
  */
 
-public class KommunicateApplication extends MultiDexApplication implements KmActionCallback, ALRichMessageListener {
+public class KommunicateApplication extends MultiDexApplication implements KmActionCallback{
 
     @Override
     protected void attachBaseContext(Context base) {
@@ -64,26 +58,6 @@ public class KommunicateApplication extends MultiDexApplication implements KmAct
             case Kommunicate.PRECHAT_LOGIN_CALL:
                 KmHelper.performLogin(context, (User) object);
                 break;
-
-        }
-    }
-
-    @Override
-    public void onAction(Context context, String action, Message message, Object object) {
-        switch (action) {
-            case "sendMessage":
-                new MessageBuilder(context).setMessage((String) object).setGroupId(message.getGroupId()).send();
-                break;
-
-            case "openUrl":
-                Intent intent = new Intent(context, PaymentActivity.class);
-                intent.putExtra("formAction", (String) object);
-                context.startActivity(intent);
-                break;
-
-            case "openActivity":
-                Intent intent1 = new Intent(context, MainActivity.class);
-                context.startActivity(intent1);
         }
     }
 }

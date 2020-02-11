4c4c5a00c79a700946678f3024e3f8ab7ed8db50
Ashish <reytum@live.com>
2020-02-11 16:38:06 +0530

    fix form submit type action

--- kommunicate/src/main/java/io/kommunicate/services/KmHttpClient.java
+++ kommunicate/src/main/java/io/kommunicate/services/KmHttpClient.java
@@ -85,10 +85,10 @@ public class KmHttpClient {
             return sb.toString();
         } catch (IOException e) {
             e.printStackTrace();
+            return e.getLocalizedMessage();
         } catch (Exception e) {
             e.printStackTrace();
+            return e.getLocalizedMessage();
         }
-        Utils.printLog(context, TAG, "Http call failed");
-        return null;
     }
 }

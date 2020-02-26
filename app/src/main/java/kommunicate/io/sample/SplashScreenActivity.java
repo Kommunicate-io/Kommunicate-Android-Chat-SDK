a6186a27eaf33feef00f05b9f8cb9b812da06363
Ashish <reytum@live.com>
2020-02-26 20:10:15 +0530

    revert SplashScreenActivity

--- app/src/main/java/kommunicate/io/sample/SplashScreenActivity.java
+++ app/src/main/java/kommunicate/io/sample/SplashScreenActivity.java
@@ -2,17 +2,11 @@ package kommunicate.io.sample;
 
 import android.content.Intent;
 import android.os.Handler;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 import com.applozic.mobicomkit.uiwidgets.conversation.activity.ConversationActivity;
-import com.applozic.mobicommons.commons.core.utils.Utils;
 
-import io.kommunicate.KmConversationBuilder;
-import io.kommunicate.app.BuildConfig;
-import io.kommunicate.callbacks.KmCallback;
 import io.kommunicate.users.KMUser;
 import io.kommunicate.app.R;
 
@@ -29,24 +23,9 @@ public class SplashScreenActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                /*Intent intent = new Intent(SplashScreenActivity.this, KMUser.isLoggedIn(SplashScreenActivity.this) ? ConversationActivity.class : MainActivity.class);
+                Intent intent = new Intent(SplashScreenActivity.this, KMUser.isLoggedIn(SplashScreenActivity.this) ? ConversationActivity.class : MainActivity.class);
                 SplashScreenActivity.this.startActivity(intent);
-                SplashScreenActivity.this.finish();*/
-
-                new KmConversationBuilder(SplashScreenActivity.this)
-                        .setWithPreChat(true)
-                        .setAppId(BuildConfig.APP_ID)
-                        .launchConversation(new KmCallback() {
-                            @Override
-                            public void onSuccess(Object message) {
-                                Utils.printLog(SplashScreenActivity.this, "TestSome", "Successfully created conversation : " + message);
-                            }
-
-                            @Override
-                            public void onFailure(Object error) {
-                                Utils.printLog(SplashScreenActivity.this, "TestSome", "Failed to create conversation : " + error);
-                            }
-                        });
+                SplashScreenActivity.this.finish();
             }
         }, SPLASH_DISPLAY_LENGTH);
     }

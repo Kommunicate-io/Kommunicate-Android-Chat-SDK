bb27070b82295507041274ba0498333884560255
Pratham02Komm <pratham.pahad@kommunicate.io>
2024-07-30 18:43:19 +0530

    [CM-2127] PreChat Labels not visible when Configured from dashboard

--- kommunicate/src/main/java/io/kommunicate/KmConversationHelper.java
+++ kommunicate/src/main/java/io/kommunicate/KmConversationHelper.java
@@ -433,23 +433,22 @@ public class KmConversationHelper {
                 }
             }
             if (conversationBuilder.isWithPreChat()) {
-                try {
-                    Kommunicate.launchPrechatWithResult(conversationBuilder.getContext(), new KmPrechatCallback<KMUser>() {
-                        @Override
-                        public void onReceive(KMUser user, Context context, ResultReceiver finishActivityReceiver) {
-                            Kommunicate.login(conversationBuilder.getContext(), user, getLoginHandler(conversationBuilder, getStartConversationHandler(conversationBuilder.isSkipConversationList(), launchConversation, conversationBuilder.getPreFilledMessage(), finishActivityReceiver, callback), callback));
+                Kommunicate.launchConversationWithPreChat(conversationBuilder.getContext(), null, new KmCallback() {
+                    @Override
+                    public void onSuccess(Object message) {
+                        if (callback != null) {
+                            callback.onSuccess(message);
                         }
-
-                        @Override
-                        public void onError(String error) {
-
-                        }
-                    });
-                } catch (KmException e) {
-                    if (callback != null) {
-                        callback.onFailure(e);
                     }
-                }
+
+                    @Override
+                    public void onFailure(Object error) {
+                        if (callback != null) {
+                            callback.onFailure(error);
+                        }
+                        Utils.printLog(conversationBuilder.getContext(), TAG, "Failed to launch conversation with pre-chat: " + error);
+                    }
+                });
             } else {
                 KMUser kmUser;
 

998582d7fbe83c0e9cc12a13ae2616ca6cba4547
Prateek Singh <prateek.singh@kommunicate.io>
2024-10-25 14:10:52 +0530

    Migrate Utility classes to Kotlin (#561)
    * Rename .java to .kt
    
    * Migrated KMAgentStatusHelper.java to Kotlin.
    
    * Rename .java to .kt
    
    * Migrated KmAppSettingPreference.java to Kotlin.
    
    * Create Annotation for code cleanup.
    
    * Rename .java to .kt
    
    * Migrated KmConstants.java to Kotlin
    
    * Rename .java to .kt
    
    * Migrated KmDateUtils.java to Kotlin
    
    * Rename .java to .kt
    
    * Migrated KmInputTextLimitUtil.java to Kotlin
    
    * Deleted KMPermissionUtils.java because Not used anywhere.
    
    * Rename .java to .kt
    
    * Migrated KmUtils.java to Kotlin
    
    * Rename .java to .kt
    
    * Migrated KmViewHelper.java to Kotlin
    
    * Fixed uplpoad image crash.
    
    * Fixed Conflicts:

---
+++ kommunicate/src/main/java/io/kommunicate/utils/KmConstants.kt
@@ -0,0 +1,46 @@
+package io.kommunicate.utils
+
+import annotations.CleanUpRequired
+
+object KmConstants {
+    const val PRECHAT_RESULT_CODE: Int = 100
+    const val PRECHAT_RESULT_FAILURE: Int = 111
+    const val PRECHAT_RESULT_RECEIVER: String = "kmPrechatReceiver"
+    const val FINISH_ACTIVITY_RECEIVER: String = "kmFinishActivityReceiver"
+    const val PRECHAT_RETURN_DATA_MAP: String = "kmPrechatReturnData"
+    const val TAKE_ORDER: String = "takeOrder"
+    const val USER_ID: String = "userId"
+    const val GROUP_ID: String = "groupId"
+    @CleanUpRequired(
+        reason = "Redundant. Already present in MobiComQuickConversationFragment"
+    )
+    const val START_NEW_CHAT: String = "startNewChat"
+    const val LOGOUT_CALL: String = "logoutCall"
+    @CleanUpRequired(
+        reason = "Not used anywhere"
+    )
+    const val PRECHAT_LOGIN_CALL: String = "prechatLogin"
+    const val KM_USER_DATA: String = "kmUserData"
+    const val KM_PREFILLED_MESSAGE: String = "kmPreFilledMessage"
+    const val CONVERSATION_ASSIGNEE: String = "CONVERSATION_ASSIGNEE"
+    const val KM_CONVERSATION_TITLE: String = "KM_CONVERSATION_TITLE"
+    const val KM_HELPCENTER_URL: String = "KM_HELPCENTER_URL"
+    const val PRECHAT_ACTIVITY_NAME: String = "com.applozic.mobicomkit.uiwidgets.kommunicate.activities.LeadCollectionActivity"
+    const val CONVERSATION_ACTIVITY_NAME: String = "com.applozic.mobicomkit.uiwidgets.conversation.activity.ConversationActivity"
+    const val STATUS_AWAY: Int = 2
+    const val STATUS_ONLINE: Int = 3
+    const val STATUS_OFFLINE: Int = 0
+    const val STATUS_CONNECTED: Int = 1
+    const val MESSAGE_CLUBBING_TIME_FRAME: Long = 300000L
+    const val NOTIFICATION_TONE: String = "com.applozic.mobicomkit.notification.tone"
+    const val CLOSE_CONVERSATION_SCREEN: String = "CLOSE_CONVERSATION_SCREEN"
+    const val AWS_ENCRYPTED: String = "AWS-ENCRYPTED-"
+    const val KM_USER_LOCALE: String = "kmUserLocale"
+    @CleanUpRequired(
+        reason = "Not used anywhere. Migrated to $KM_USER_LOCALE"
+    )
+    const val KM_USER_LANGUAGE_CODE: String = "kmUserLanguageCode"
+    const val HIDDEN: String = "hidden"
+    const val PSEUDONAME: String = "pseudoName"
+    const val KM_PSEUDO_USER: String = "KM_PSEUDO_USER"
+}

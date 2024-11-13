cf53bf4f93ea24cf969413ece287d7d6827ef17f
Prateek Singh <prateek.singh@kommunicate.io>
2024-11-13 18:01:12 +0530

    Removed unused dependencies.

--- kommunicateui/src/main/java/com/applozic/mobicomkit/uiwidgets/async/FileTaskAsync.java
+++ kommunicateui/src/main/java/com/applozic/mobicomkit/uiwidgets/async/FileTaskAsync.java
@@ -1,28 +1,15 @@
 package com.applozic.mobicomkit.uiwidgets.async;
 
-import static android.os.Environment.getExternalStoragePublicDirectory;
-
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.AsyncTask;
-import android.os.Environment;
 import android.text.TextUtils;
-import android.util.Log;
 
 import com.applozic.mobicomkit.api.attachment.FileClientService;
 import com.applozic.mobicomkit.uiwidgets.kommunicate.callbacks.PrePostUIMethods;
 import com.applozic.mobicommons.file.FileUtils;
-import com.iceteck.silicompressorr.SiliCompressor;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.Locale;
 
 /**
  * a async task that will write the given file with the given "uri" to the "file"

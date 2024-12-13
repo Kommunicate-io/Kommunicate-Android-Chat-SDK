72903bc021e9753287a79e5271b60e969748115d
Prateek Singh <prateek.singh@kommunicate.io>
2024-12-13 18:30:58 +0530

    Migrated KmUserLoginTask and UserLoginTask to Coroutines (#577)
    * Migrated KmUserLoginTask and UserLoginTask to Coroutines
    
    * Marked migrated async tasks to depricated
    
    * Moved the Usecases to kommunicate module
    
    * Removed the redundant login calls
    
    * Added Kdoc

---
+++ kommunicate/src/main/java/io/kommunicate/usecase/KMUserLoginUseCase.kt
@@ -0,0 +1,105 @@
+package io.kommunicate.usecase
+
+import android.content.Context
+import android.os.ResultReceiver
+import com.applozic.mobicomkit.Applozic
+import com.applozic.mobicomkit.api.account.register.RegisterUserClientService
+import com.applozic.mobicomkit.api.account.register.RegistrationResponse
+import com.applozic.mobicomkit.listners.AlLoginHandler
+import io.kommunicate.services.KmUserClientService
+import io.kommunicate.users.KMUser
+import io.kommunicate.utils.APIResult
+import io.kommunicate.utils.KmAppSettingPreferences
+import io.kommunicate.utils.KmConstants
+import io.kommunicate.utils.UseCaseExecutor
+import io.kommunicate.utils.onFailure
+import io.kommunicate.utils.onSuccess
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+/**
+ * Use case for handling the login or registration of a KMUser in the Kommunicate SDK.
+ *
+ * @property context The Android context for accessing resources and services.
+ * @property user The [KMUser] object containing user details for login or registration.
+ * @property isAgent Boolean flag indicating if the user is an agent or a regular user.
+ * @property preChatReceiver Optional [ResultReceiver] to handle pre-chat events.
+ */
+class KMUserLoginUseCase(
+    private val context: Context,
+    private val user: KMUser,
+    private val isAgent: Boolean,
+    private val preChatReceiver: ResultReceiver? = null
+): UseCase<APIResult<RegistrationResponse>> {
+
+    private val userClientService: KmUserClientService = KmUserClientService(context)
+    private val registerUserClientService: RegisterUserClientService = RegisterUserClientService(context)
+
+    override suspend fun execute(): APIResult<RegistrationResponse> = withContext(Dispatchers.IO) {
+        try {
+            val response = if (isAgent) {
+                userClientService.clearDataAndPreference()
+                userClientService.loginKmUser(user)
+            } else {
+                userClientService.clearDataAndPreference()
+                KmAppSettingPreferences.fetchAppSetting(context, Applozic.getInstance(context).applicationKey)
+                registerUserClientService.createAccount(user)
+            }
+
+            preChatReceiver?.send(KmConstants.PRECHAT_RESULT_CODE, null)
+
+            if (response.isRegistrationSuccess) {
+                APIResult.success(response)
+            } else {
+                APIResult.failed("unable to login user on server.")
+            }
+        } catch (e: Exception) {
+            APIResult.failedWithException(e)
+        }
+    }
+
+    companion object {
+
+        /**
+         * Executes the use case with the given [UseCaseExecutor]. This helper function invokes the
+         * coroutine internally don't invoke explicitly.
+         *
+         * This method provides a simpler way to execute the use case and handle success or failure
+         * using a provided [AlLoginHandler].
+         *
+         * @param context The Android context for accessing resources and services.
+         * @param user The [KMUser] object containing user details for login or registration.
+         * @param isAgent Boolean flag indicating if the user is an agent or a regular user.
+         * @param preChatReceiver Optional [ResultReceiver] to handle pre-chat events.
+         * @param loginHandler Optional [AlLoginHandler] for handling login success or failure callbacks.
+         * @return [UseCaseExecutor] for cancelling the coroutine.
+         */
+        fun executeWithExecutor(
+            context: Context,
+            user: KMUser,
+            isAgent: Boolean,
+            preChatReceiver: ResultReceiver? = null,
+            loginHandler: AlLoginHandler? = null,
+        ): UseCaseExecutor<KMUserLoginUseCase, APIResult<RegistrationResponse>> {
+
+            val kmUserLoginUseCase = KMUserLoginUseCase(context, user, isAgent, preChatReceiver)
+            val executor = UseCaseExecutor(
+                kmUserLoginUseCase,
+                { result: APIResult<RegistrationResponse> ->
+                    result.onSuccess { registrationResponse: RegistrationResponse? ->
+                        loginHandler?.onSuccess(registrationResponse, context)
+                    }
+                    result.onFailure {
+                        loginHandler?.onFailure(null, it)
+                    }
+                },
+                { exception: java.lang.Exception? ->
+                    loginHandler?.onFailure(null, exception)
+                },
+                Dispatchers.IO
+            )
+            executor.invoke()
+            return executor
+        }
+    }
+}
---
+++ kommunicate/src/main/java/io/kommunicate/usecase/UserLoginUseCase.kt
@@ -0,0 +1,90 @@
+package io.kommunicate.usecase
+
+import android.content.Context
+import com.applozic.mobicomkit.api.account.register.RegisterUserClientService
+import com.applozic.mobicomkit.api.account.register.RegistrationResponse
+import com.applozic.mobicomkit.api.account.user.User
+import com.applozic.mobicomkit.api.account.user.UserClientService
+import com.applozic.mobicomkit.listners.AlLoginHandler
+import io.kommunicate.utils.APIResult
+import io.kommunicate.utils.UseCaseExecutor
+import io.kommunicate.utils.onFailure
+import io.kommunicate.utils.onSuccess
+import kotlinx.coroutines.Dispatchers
+
+/**
+ * A use case for logging in or registering a user in the Kommunicate SDK.
+ *
+ * This class handles user login/registration by interacting with the Applozic user management services.
+ *
+ * @property user The [User] object containing the user details for login or registration.
+ * @constructor Initializes the use case with the provided [Context] and [User].
+ */
+class UserLoginUseCase(
+    context: Context,
+    private val user: User,
+): UseCase<APIResult<RegistrationResponse>> {
+
+    private val userClientService: UserClientService = UserClientService(context)
+    private val registerUserClientService: RegisterUserClientService = RegisterUserClientService(context)
+
+    /**
+     * Executes the login or registration operation for the provided user.
+     *
+     * - Clears any existing user data and preferences before attempting registration.
+     * - Registers the user using [RegisterUserClientService].
+     *
+     * @return [APIResult] containing the [RegistrationResponse] if successful, or an error message/exception.
+     */
+    override suspend fun execute(): APIResult<RegistrationResponse> {
+        return try {
+            userClientService.clearDataAndPreference()
+            val registrationResponse = registerUserClientService.createAccount(user)
+            if (registrationResponse.isRegistrationSuccess) {
+                APIResult.success(registrationResponse)
+            } else {
+                APIResult.failed("unable to register user on server.")
+            }
+        } catch (e: Exception) {
+            APIResult.failedWithException(e)
+        }
+    }
+
+    companion object {
+
+        /**
+         * Executes the use case with a [UseCaseExecutor] for asynchronous processing.
+         *
+         * This method simplifies the use of the use case by providing callbacks for success and failure.
+         *
+         * @param context The Android [Context] for accessing resources and services.
+         * @param user The [User] object containing the user details for login or registration.
+         * @param loginHandler An optional [AlLoginHandler] to handle the success or failure callbacks.
+         * @return A [UseCaseExecutor] instance for managing the execution of the use case.
+         */
+        fun executeWithExecutor(
+            context: Context,
+            user: User,
+            loginHandler: AlLoginHandler?
+        ): UseCaseExecutor<UserLoginUseCase, APIResult<RegistrationResponse>> {
+            val userLoginUseCase = UserLoginUseCase(context, user)
+            val executor = UseCaseExecutor(
+                userLoginUseCase,
+                { result: APIResult<RegistrationResponse> ->
+                    result.onSuccess { registrationResponse: RegistrationResponse? ->
+                        loginHandler?.onSuccess(registrationResponse, context)
+                    }
+                    result.onFailure {
+                        loginHandler?.onFailure(null, it)
+                    }
+                },
+                { exception: java.lang.Exception? ->
+                    loginHandler?.onFailure(null, exception)
+                },
+                Dispatchers.IO
+            )
+            executor.invoke()
+            return executor
+        }
+    }
+}

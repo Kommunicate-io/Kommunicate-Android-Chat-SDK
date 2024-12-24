314d2a0d9f0783bf65beb4adaed5074b294f88ee
Prateek Singh <prateek.singh@kommunicate.io>
2024-12-24 10:16:34 +0530

    Added flag to disable SSL Pinning in Android. (#581)
    * Added flag to disable SSL Pinning in Android.
    
    * Updated test cases
    
    * Revert "Updated test cases"
    
    This reverts commit 8e4af07afb01f28aef64246d6a6990d4e6a24f51.
    
    * Created function to disable SSL Pinning

--- kommunicate/src/main/java/io/kommunicate/utils/KmAppSettingPreferences.kt
+++ kommunicate/src/main/java/io/kommunicate/utils/KmAppSettingPreferences.kt
@@ -29,6 +29,7 @@ object KmAppSettingPreferences {
     private const val UPLOAD_OVERRIDE_HEADER = "UPLOAD_OVERRIDE_HEADER"
     private const val SINGLE_THREADED = "IS_SINGLE_THREADED"
     private const val ROOT_DETECTION = "ROOT_DETECTION"
+    private const val SSL_PINNING = "SSL_PINNING"
     private const val RATING_BASE = "RATING_BASE"
 
     @JvmStatic
@@ -51,8 +52,16 @@ object KmAppSettingPreferences {
             .getSharedPreferences(MobiComUserPreference.AL_USER_PREF_KEY, Context.MODE_PRIVATE)
     }
 
+    @JvmStatic
     val isRootDetectionEnabled: Boolean
         get() = preferences.getBoolean(ROOT_DETECTION, true)
+
+    @JvmStatic
+    var isSSLPinningEnabled: Boolean
+        get() = preferences.getBoolean(SSL_PINNING, false)
+        set(isEnabled) {
+            preferences.edit().putBoolean(SSL_PINNING, isEnabled).apply()
+        }
 
     var primaryColor: String?
         get() = preferences.getString(KM_THEME_PRIMARY_COLOR, null)

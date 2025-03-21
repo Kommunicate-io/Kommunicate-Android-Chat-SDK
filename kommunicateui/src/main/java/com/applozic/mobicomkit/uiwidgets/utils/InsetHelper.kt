5284692086ce6e5afb508e324d694df572633fdc
Prateek Singh <prateek.singh@kommunicate.io>
2025-03-21 18:01:30 +0530

    Added function to resolve keyboard

--- kommunicateui/src/main/java/com/applozic/mobicomkit/uiwidgets/utils/InsetHelper.kt
+++ kommunicateui/src/main/java/com/applozic/mobicomkit/uiwidgets/utils/InsetHelper.kt
@@ -1,15 +1,19 @@
 package com.applozic.mobicomkit.uiwidgets.utils
 
+import android.animation.ValueAnimator
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.DecelerateInterpolator
+import androidx.core.animation.doOnEnd
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
 import androidx.core.view.updatePadding
 
 /**
- * A utility object that helps manage system insets (like status bar, navigation bar, etc.)
+ * A utility object that helps manage system insets (like status bar, navigation bar, keyboard, etc.)
  * by configuring padding or margin adjustments based on the system window insets.
+ * Includes support for keyboard animations and edge-to-edge display.
  */
 object InsetHelper {
 
@@ -30,6 +34,12 @@ object InsetHelper {
      */
     @JvmField
     val navigationTypeMask = WindowInsetsCompat.Type.navigationBars()
+
+    /**
+     * [imeTypeMask] is a type mask for the software keyboard.
+     */
+    @JvmField
+    val imeTypeMask = WindowInsetsCompat.Type.ime()
 
     /**
      * Configures the system insets with status bar to update either
@@ -53,7 +63,18 @@ object InsetHelper {
         bottom: Int = -1,
         isPadding: Boolean = true
     ) {
-        configureInset(view, systemTypeMask, 0 , 0, top, bottom, isPadding)
+        configureInset(view, systemTypeMask, 0, 0, top, bottom, isPadding)
+    }
+
+    @JvmStatic
+    fun configureSystemInsetsWithKeyboard(
+        view: View,
+        top: Int = -1,
+        bottom: Int = -1,
+        isPadding: Boolean = true,
+        adjustForKeyboard: (isVisible: Boolean, keyboardHeight: Int) -> Unit
+    ) {
+        configureInset(view, systemTypeMask, 0, 0, top, bottom, isPadding, adjustForKeyboard)
     }
 
     /**
@@ -73,6 +94,7 @@ object InsetHelper {
      * @param top The padding or margin to apply on the top side. Default is -1 (which means apply the inset value).
      * @param bottom The padding or margin to apply on the bottom side. Default is -1 (which means apply the inset value).
      * @param isPadding If true, the function adjusts the padding of the view. If false, it adjusts the margins of the view. Default is true.
+     * @param adjustForKeyboard  the function will adjust the view when the keyboard appears/disappears.
      */
     @JvmStatic
     fun configureInset(
@@ -82,23 +104,43 @@ object InsetHelper {
         right: Int = -1,
         top: Int = -1,
         bottom: Int = -1,
-        isPadding: Boolean = true
+        isPadding: Boolean = true,
+        adjustForKeyboard: ((isVisible: Boolean, keyboardHeight: Int) -> Unit)? = null
     ) {
+        var lastKeyboardHeight = 0
+        var wasKeyboardVisible = false
+
         ViewCompat.setOnApplyWindowInsetsListener(view) { targetView, windowInsets ->
             val insets = windowInsets.getInsets(typeMask)
+            val imeInsets = windowInsets.getInsets(imeTypeMask)
+            val isKeyboardVisible = windowInsets.isVisible(WindowInsetsCompat.Type.ime())
+
+            // Combine insets if adjusting for keyboard
+            val bottomInset = if (adjustForKeyboard != null && imeInsets.bottom > 0) {
+                imeInsets.bottom
+            } else {
+                insets.bottom
+            }
+
+            if (isKeyboardVisible != wasKeyboardVisible) {
+                adjustForKeyboard?.invoke(isKeyboardVisible, lastKeyboardHeight)
+                wasKeyboardVisible = isKeyboardVisible
+                lastKeyboardHeight = resolveInset(bottom, bottomInset)
+            }
+
             if (isPadding) {
                 targetView.updatePadding(
                     left = resolveInset(left, insets.left),
                     right = resolveInset(right, insets.right),
                     top = resolveInset(top, insets.top),
-                    bottom = resolveInset(bottom, insets.bottom)
+                    bottom = resolveInset(bottom, bottomInset)
                 )
             } else {
                 targetView.updateLayoutParams<ViewGroup.MarginLayoutParams> {
                     leftMargin = resolveInset(left, insets.left)
                     rightMargin = resolveInset(right, insets.right)
-                    bottomMargin = resolveInset(bottom, insets.bottom)
                     topMargin = resolveInset(top, insets.top)
+                    bottomMargin = resolveInset(bottom, bottomInset)
                 }
             }
             // Return CONSUMED to prevent further insets handling by the system
